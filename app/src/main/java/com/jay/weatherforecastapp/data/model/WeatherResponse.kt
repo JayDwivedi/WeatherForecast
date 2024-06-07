4a9bc18dfbf6ef5f568abaad3fb65682bb072d18
Jay Dwivedi <jaidwivedi21@gmail.com>
2024-06-07 06:05:05 +0530

    Add Folder structure Add API classes interface and response

--- app/src/main/java/com/jay/weatherforecastapp/data/model/WeatherResponse.kt
+++ app/src/main/java/com/jay/weatherforecastapp/data/model/WeatherResponse.kt
@@ -1,3 +1,15 @@
 package com.jay.weatherforecastapp.data.model
 
-data class WeatherResponse()
+data class WeatherResponse(
+    val temperature: String,
+    val wind: String,
+    val description: String,
+    val forecast: List<Forecast>
+)
+
+data class Forecast(
+    val day: Int,
+    val temperature: String,
+    val wind: String
+)
+
--- app/src/main/java/com/jay/weatherforecastapp/data/remote/WeatherApiService.kt
+++ app/src/main/java/com/jay/weatherforecastapp/data/remote/WeatherApiService.kt
@@ -1,4 +1,10 @@
 package com.jay.weatherforecastapp.data.remote
 
+import com.jay.weatherforecastapp.data.model.WeatherResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface WeatherApiService {
+    @GET("weather/{city}")
+    suspend fun getWeather(@Path("city") city: String): WeatherResponse
 }
